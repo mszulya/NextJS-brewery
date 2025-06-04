026f9330af79c36e97604b11c929910f2f028b69
zulya <sul.zulya@gmail.com>
2025-06-04 10:11:46 +1000

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,20 @@ import Image from "next/image"
 import {useEffect, useState} from 'react'
 import {useRouter} from 'next/navigation'
 
+type Brewery = {
+    id: string
+    name: string
+    brewery_type: string
+    city: string
+    state: string
+    country: string
+    phone: string
+    website_url: string
+}
+
 export default function Home() {
     const [search, setSearch] = useState('')
-    const [answer, setAnswer] = useState([])
+    const [answer, setAnswer] = useState<Brewery[]>([])
     const router = useRouter()
 
     useEffect(() => {
